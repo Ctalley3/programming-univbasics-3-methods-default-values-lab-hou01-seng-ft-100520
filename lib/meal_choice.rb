80240b6988287ace14eafbabddd76b1bac00c687
Crystal T <ctalley.cat@gmail.com>
2020-09-22 01:49:00 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,3 @@
-Meal_choice ('carrots', "string bean", "lentils")
 
 def meal_choice(veg1, veg2, protien ='tofu')
 end
@@ -7,7 +6,7 @@ def meal_choice(veg1, veg2, protien ='tofu')
  puts "what a nutritious meal"
 end
 
-def meal_choice(veg1, veg2, protien ='tofu')
+def meal_choice ('carrots', "string bean", "lentils")
  puts "what a nutritious meal"
  puts "a plate of #{protein} with #{veg1} and {veg2}."
  
